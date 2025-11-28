a8193a3aae41308a5e717fe479d489fe3a458580
Carlo Yañez <carloyanez764@gmail.com>
2025-11-28 12:57:05 -0600

    Endpoint para condicion del auto. Se maneja de forma dinámica los campos

--- src/main/java/com/example/demo/controller/AutoController.java
+++ src/main/java/com/example/demo/controller/AutoController.java
@@ -5,12 +5,14 @@ import com.example.demo.model.Condicion_auto;
 import com.example.demo.service.AutoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -23,6 +25,35 @@ public class AutoController {
 
     @PostMapping("/condicion/registrar")
     public ResponseEntity<?> registrarCondicion(@RequestBody CondicionAutoDTO dto) {
+        Map<String, Object> response = new HashMap<>();
+
+        if (dto.getAuto_id() == null) {
+            response.put("data", Map.of("error", "El id del auto es obligatorio."));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+
+        if (dto.getDescripcion() == null || dto.getDescripcion().trim().isEmpty()) {
+            response.put("data", Map.of("error", "La descripción es obligatoria."));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+
+        if (dto.getObservaciones() == null || dto.getObservaciones().trim().isEmpty()) {
+            response.put("data", Map.of("error", "Las observaciones son obligatorias."));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+
+
+        if (dto.getKilometraje() == null) {
+            response.put("data", Map.of("error", "El kilometraje es obligatorio."));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+
+
+        if (dto.getDetalles() == null || dto.getDetalles().isEmpty()) {
+            response.put("data", Map.of("error", "Las condiciones del auto son obligatorias."));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+
         Condicion_auto guardado = autoService.registrarCondicion(dto);
 
         return ResponseEntity.ok(Map.of(
